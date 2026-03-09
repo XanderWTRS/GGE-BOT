9c31cd2b8b37e35aa55addcac7e56ae898240df5
darrenthebozz <darrenthebozz1@gmail.com>
2026-03-09 03:54:02 +0000

    redirect directory to script location

--- start_linux.sh
+++ start_linux.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 URL="http://127.0.0.1:3001"
-
+cd "$(dirname "$0")"
 if [ ! -d ".git" ]; then
   git init -b main 
   git remote add origin https://github.com/darrenthebozz/GGE-BOT.git
