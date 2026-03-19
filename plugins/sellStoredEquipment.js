af18d8618ff25fbe85d3d5f008d801b345e001cb
darrenthebozz <darrenthebozz1@gmail.com>
2026-03-19 08:39:25 +0000

    Sell stored equipment (Shouldnt sell unique/relic)

---
+++ plugins/sellStoredEquipment.js
@@ -0,0 +1,74 @@
+const { events, xtHandler, sendXT } = require('../ggeBot')
+
+if (require('node:worker_threads').isMainThread)
+    return module.exports = {}
+
+const gems = require("../items/gems.json")
+xtHandler.on("ggm", e => {
+    let gemsSold = 0
+    Array.from(e.GEM).map(([id, ammount]) => ({id, ammount})).forEach(({id, ammount}) => {
+        const gem = gems.find(e => e.gemID == id)
+        if(gem.setID != undefined)
+            return
+
+        for (let i = 0; i < ammount; i++) {
+            sendXT("sge", JSON.stringify({GID:gem.gemID,RGEM:0,LFID:-1}))
+            gemsSold++
+        }
+    })
+
+    console.log(gemsSold, 'gemsSold')
+})
+const equipmentType = {
+    unique : 0,
+    common : 1,
+    rare : 2,
+    epic : 3,
+    legendary : 4,
+    relic : 5,
+    heroUnique : 10, 
+    heroCommon : 11,
+    heroRare : 12,
+    heroEpic : 13,
+    heroLegendary : 14,
+    heroRelic : 15
+}
+class Equipment {
+    constructor(e) {
+        this.id = e[0]
+        this.slotType = e[1]
+        this.lordType = e[2]
+        this.rarity = e[3]
+        this.name = e[4]
+        this.objectID = e[6]
+        this.setID = e[7]
+        this.enchantmentLevel = e[8]
+        this.timeLeft = e[9] + Date.now() //TODO Verify
+        this.temporary = e[9] > 0
+        this.gemID = e[10]
+    }
+}
+
+xtHandler.on("gei", e => {
+    let equipmentSold = 0
+    Array.from(e.I).map(e => new Equipment(e)).forEach(equipment => {
+        if([equipmentType.relic, equipmentType.heroRelic, equipmentType.unique, equipmentType.heroUnique]
+            .includes(equipment.rarity))
+            return
+        if(equipment.setID != -1)
+            return
+
+        sendXT("seq", JSON.stringify({EID:equipment.id, LID:-1, EX:0, LFID:-1}))
+        equipmentSold++
+    })
+    console.log("equipmentSold", equipmentSold)
+})
+
+events.on("load", () => {
+    sendXT("ggm", JSON.stringify({}))
+    sendXT("gei", JSON.stringify({}))
+    setInterval(() => {
+        sendXT("ggm", JSON.stringify({}))
+        sendXT("gei", JSON.stringify({}))
+    }, 1000 * 10 * 30)
+})
